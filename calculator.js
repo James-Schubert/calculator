f814bece7128d82056accbe2c6dedce804a09b84
James Schubert <jvs@vt.edu>
2022-07-14 21:43:58 -0400

    adds basic functionality

--- calculator.js
+++ calculator.js
@@ -1,18 +1,63 @@
-function add(num1, num2){
-    return num1+num2;
+function add(arr){
+    arr = arr.split("+");
+    let sum = 0;
+    arr.forEach((subArr) =>{
+        if (subArr.includes("-")){
+            subArr = subtract(subArr);
+        }
+        else if(subArr.includes("\u00d7")){
+            subArr = multiply(subArr);
+        }
+        else if(subArr.includes("\u00f7")){
+            subArr = divide(subArr);
+        }
+        sum += parseInt(subArr);
+    })
+    return sum;
 }
 
-function subtract(num1, num2){
-    return num1-num2;
+function subtract(arr){
+    arr = arr.split("-");
+    let difference = arr[0];
+    for(let i = 1; i<arr.length;i++){
+        let subArr = arr[i];
+        if (subArr.includes("+")){
+            subArr = add(subArr);
+        }
+        else if(subArr.includes("\u00d7")){
+            subArr = multiply(subArr);
+        }
+        else if(subArr.includes("\u00f7")){
+            subArr = divide(subArr);
+        }
+        difference -= parseInt(subArr);
+    }
+    return difference;
 }
 
-function multiply(num1, num2){
-    return num1*num2;
+function multiply(arr){
+    arr = arr.split("\u00d7");
+    let prod = 1;
+    arr.forEach((subArr) =>{
+        if(subArr.includes("\u00f7")){
+            subArr = divide(subArr);
+        }
+        prod *= parseInt(subArr);
+    })
+    return prod;
 }
 
 function divide(num1, num2){
-    if(num2 == 0) return "ERROR: DIVISION BY ZERO";
-    else return num1/num2;
+    arr = arr.split("\u00f7");
+    let div = arr[0];
+    for(let i = 1; i< arr.length; i++){
+        let subArr  =arr[i];
+        if(subArr.includes("\u00d7")){
+            subArr = multiply(subArr);
+        }
+        div /= parseInt(subArr);
+    }
+    return div;
 }
 
 function exponent(num1, num2){
@@ -30,16 +75,61 @@ const buttonBox = document.createElement('div');
 buttonBox.setAttribute("id","buttonBox");
 
 let screenVals = "";
+let lastInput = "";
+let operators = "+=\u00f7\u00d7";
+let lastResult = ">>>>>>>>>";
 
 function addListeners(div){
     div.addEventListener("click",()=>{
-        screenVals += div.textContent;
-        upDateScreen();
-        console.log(div.id);
+        if(div.id == "AC"){
+            screenVals = '';
+        }
+        else if(div.id == "C"){
+            let lastInd = screenVals.length-lastInput.length;
+            screenVals = screenVals.slice(0,lastInd);
+        }
+        else if(div.id == "equalButton") calculate();
+        else{
+            
+            screenVals += div.textContent;
+            if (lastInput != "+" && lastInput != "\u00f7" && lastInput != "-" && lastInput != "\u00d7" && !operators.includes(div.textContent)){
+                lastInput += div.textContent;
+            }
+            else lastInput =div.textContent;
+        }
+        updateScreen();
+        console.log(lastInput);
     });
 }
 
-function upDateScreen(){
+function calculate(){
+    let lastline = screenVals.split(lastResult);
+    lastline = lastline[lastline.length-1];
+    if(operators.includes(lastline.charAt(0))){
+        lastline = parseInt(lastResult.split("="))+lastline;
+    }
+    result = add(lastline);
+    lastResult = "="+result;
+    //result = 8008;
+    displayResult(Result);
+}
+function displayResult(result){
+    screenVals +=`\n=${result}\n`;
+    lastInput = `=${result}`;
+    updateScreen();
+}
+
+// function pad(value, length) {
+//     return (value.toString().length < length) ? pad("\u0020"+value, length):value;
+// }
+
+
+function updateScreen(){
+    console.log(screenVals);
+    let screenArr = screenVals.split('\n');
+    if(screenArr.length >3){
+        screenVals = screenArr[1]+"\n"+screenArr[2]+"\n"+screenArr[3];
+    }
     screen.textContent = screenVals;
 }
 
@@ -72,6 +162,8 @@ calcBody.appendChild(screen);
 container.appendChild(calcBody);
 calcBody.appendChild(buttonBox);
 container.appendChild(calcBody);
+
+
 const row5 = document.querySelector("#row5");
 const equalButton = row5.lastChild;
 equalButton.setAttribute("id","equalButton");
